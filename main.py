037762dff4001743a01a92defe6fc669bb94af7a
Zahra Oveisi <z.oveisi@ebcom.ir>
2023-06-19 09:12:14 +0330

    weather api creation

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
+
 @app.route("/")
 def home():
     return render_template("home.html")
 
 @app.route("/api/v1/<station>/<date>")
 def api(station, date):
-    temprature = 23
+    filename = "data-small/TG_STAID"+str(station).zfill(6)+".txt"
+    df = pd.read_csv(filename, skiprows=20,parse_dates=['    DATE'])
+    temprature = df.loc[df['    DATE']==date]['   TG'].squeeze()/10
     return {"station":station,
             "date":date,
             "temprature":temprature}
 
+
 if __name__=="__main__":
     app.run(debug=True)
+
