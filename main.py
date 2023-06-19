059398408ff1ddda8d829912c3bd815780ea8f7c
Zahra Oveisi <z.oveisi@ebcom.ir>
2023-06-19 17:34:25 +0330

    add table for home page

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import pandas as pd
 
 app = Flask(__name__)
 
+stations = pd.read_csv('data-small/stations.txt', skiprows=17)
+stations = stations[["STAID","STANAME                                 "]]
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data = stations.to_html())
 
 @app.route("/api/v1/<station>/<date>")
 def api(station, date):
@@ -20,3 +23,4 @@ def api(station, date):
 if __name__=="__main__":
     app.run(debug=True)
 
+print(stations)
